a3fe04a2921bb705d899d0cd47da0a1ac4fe0bb7
Pavel Vasev <pavel.vasev@gmail.com>
2021-04-08 12:25:46 +0500

    color serie

--- clip/init.js
+++ clip/init.js
@@ -2,10 +2,12 @@ import a1 from "./shaderclip.js";
 import a2 from "./shaderclipserie.js";
 import a3 from "./shaderclipcolor.js";
 import a4 from "./shaderclipscreen.js";
+import a5 from "./shadercolorserie.js";
 
 export default function setup(m) {
   a1(m);
   a2(m);
   a3(m);
   a4(m);
+  a5(m);
 }
