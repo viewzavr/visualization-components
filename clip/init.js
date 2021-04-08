fe4bc1060afcf94e908022f10a1cf1ba099fd479
Pavel Vasev <pavel.vasev@gmail.com>
2021-04-08 12:15:27 +0500

    clip to screen

--- clip/init.js
+++ clip/init.js
@@ -1,9 +1,11 @@
 import a1 from "./shaderclip.js";
 import a2 from "./shaderclipserie.js";
 import a3 from "./shaderclipcolor.js";
+import a4 from "./shaderclipscreen.js";
 
 export default function setup(m) {
   a1(m);
   a2(m);
   a3(m);
+  a4(m);
 }
