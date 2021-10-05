86302717b6908aa348166affe804723c1aa4f5f3
Pavel Vasev <pavel.vasev@gmail.com>
2021-10-06 00:08:45 +0500

    arrows

--- visual/basis.js
+++ visual/basis.js
@@ -4,6 +4,7 @@ export default function setup( vz ) {
   vz.addItemType( "points","Points",function(opts) { return make( vz, opts ) },{cat:"visual"}  );
   vz.addItemType( "lines","Lines",function(opts) { return make_lines( vz, opts ) },{cat:"visual"}  );
   vz.addItemType( "linestrip","Linestrip",function(opts) { return make_linestrip( vz, opts ) },{cat:"visual"}  );
+  vz.addItemType( "arrows","Arrows",function(opts) { return make_arrows( vz, opts ) },{cat:"visual"}  );
 }
 
 function make( vz, opts ) {
@@ -17,6 +18,14 @@ function make( vz, opts ) {
 
 function make_lines( vz, opts ) {
   var obj = vz.vis.addLines( opts.parent, opts.name, opts );
+  
+  add_std( obj );
+
+  return obj;
+}
+
+function make_arrows( vz, opts ) {
+  var obj = vz.vis.addViewlang( "Arrows",{name:"arrows", ...opts} );
   
   add_std( obj );
 
@@ -41,6 +50,11 @@ function add_std( obj ) {
     obj.radiuses = v;
   } );
   obj.setParamOption("radiuses","internal",true);  
+  
+  obj.addArray("colors",[],1,function(v) {
+    obj.colors = v;
+  } );
+  obj.setParamOption("colors","internal",true);    
   
   obj.addCheckbox("visible",true,(v) => {
     obj.visible=v;
